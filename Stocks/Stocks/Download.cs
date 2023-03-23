917f76d1560133afaa4a68217dc7b77e375e013c
Daniel Vojnar <danda.vojnar@seznam.cz>
2023-03-23 21:35:06 +0100

    Get csv file

--- Stocks/Stocks/Download.cs
+++ Stocks/Stocks/Download.cs
@@ -2,5 +2,17 @@
 
 public class Download
 {
-    
+    private static HttpClient _client = new HttpClient();
+
+    static async Task<string?> GetEtfHoldingsCsv(string path)
+    {
+        string? csv = null;
+        HttpResponseMessage responseMessage = await _client.GetAsync(path);
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            csv = await responseMessage.Content.ReadAsStringAsync();
+        }
+        return csv;
+    }
+
 }
