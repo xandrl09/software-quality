bc7a3d2955cf35ad7534261083a4cdf08efbab87
Jaroslav Hort <hortjaroslav0@gmail.com>
2023-03-25 18:12:18 +0100

    feat: create stock model

---
+++ Stocks/Stocks/Models/StockModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stocks.Models
+{
+    public class StockModel
+    {
+        public DateTime Date { get; init; }
+        public string Company { get; init; }
+        public string Ticker { get; init; }
+        public int Shares { get; init; } 
+        public string MarketValue { get; init; }
+    }
+}
