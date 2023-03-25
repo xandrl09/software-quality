e61619260503222403e0c196a5116a396816de4c
Jaroslav Hort <hortjaroslav0@gmail.com>
2023-03-25 17:34:39 +0100

    fix: add user-agent header to download request

--- Stocks/Stocks/HttpClientArk/Download.cs
+++ Stocks/Stocks/HttpClientArk/Download.cs
@@ -1,17 +1,35 @@
-﻿namespace Stocks.HttpClientArk;
+﻿using System.IO;
+
+namespace Stocks.HttpClientArk;
 
 public class Download
 {
     private HttpClient _client;
+    private const string USER_AGENT = "StocksService/1.0";
 
     public Download(HttpClient client)
     {
         _client = client;
     }
+
+    private HttpRequestMessage CreateGetRequestMessage(string url)
+    {
+        var request = new HttpRequestMessage()
+        {
+            RequestUri = new Uri(url),
+            Method = HttpMethod.Get
+        };
+        request.Headers.Add("User-Agent", USER_AGENT);
+        return request;
+    }
+
     public async Task<string?> GetEtfHoldingsCsv(string path)
     {
         string? csv = null;
-        HttpResponseMessage responseMessage = await _client.GetAsync(path);
+
+        var requestMessage = CreateGetRequestMessage(path);
+        HttpResponseMessage responseMessage = await _client.SendAsync(requestMessage);
+        
         if (responseMessage.IsSuccessStatusCode)
         {
             csv = await responseMessage.Content.ReadAsStringAsync();
