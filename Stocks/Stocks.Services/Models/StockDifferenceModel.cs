9f4a239cada1ea36286afa5a475c10847d7852ea
Petr Balazy <balazypetr@gmail.com>
2023-03-26 19:12:55 +0200

    Add StockDifferenceModel to reflect change in individual stocks

---
+++ Stocks/Stocks.Services/Models/StockDifferenceModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stocks.Services.Models
+{
+    public class StockDifferenceModel
+    {
+        public string CompanyName { get; set; }
+        public string Ticker { get; set; }
+        public int DifferenceInShares { get; set; }
+        public string Weight { get; set; }
+    }
+}
