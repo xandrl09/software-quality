a7257e65c8722eca94a6d0f086c6400d4b69942e
Jaroslav Hort <hortjaroslav0@gmail.com>
2023-03-26 08:42:36 +0200

    feat: helper class for file path formatting

---
+++ Stocks/Stocks/Helpers/PathHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stocks.Helpers
+{
+    public static class PathHelper
+    {
+        public static string GetDateFilePath (DateTime date,
+            string format,
+            string directory,
+            string extension)
+        {
+            string path = string.Format("{0:" + format + "}", date);
+            path = Path.Join(directory, path + extension);
+            return path;
+        }
+    }
+}
