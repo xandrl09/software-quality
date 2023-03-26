3afa5994799277cc218288b23eeee6b2d57ca0f0
Petr Balazy <balazypetr@gmail.com>
2023-03-26 19:16:12 +0200

    Add HoldingsDifferenceModel to reflect changes in holding

---
+++ Stocks/Stocks.Services/Models/HoldingsDifferenceModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Stocks.Services.Models
+{
+    public class HoldingsDifferenceModel
+    {
+        public IEnumerable<StockModel> NewPositions { get; set; }
+        public IEnumerable<StockDifferenceModel> IncreasedPositons { get; set; }
+        public IEnumerable<StockDifferenceModel> ReducedPositions { get; set; }
+    }
+}
