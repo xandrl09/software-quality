63e39ca64b801760d9cb45542ec8857fd73ae4a4
Jaroslav Hort <hortjaroslav0@gmail.com>
2023-04-04 06:15:45 +0200

    fix: reading blank line from csv throws exception

--- Stocks/Stocks.Services/Parsers/CsvParseService.cs
+++ Stocks/Stocks.Services/Parsers/CsvParseService.cs
@@ -10,10 +10,16 @@ public class CsvParseService : IParseService
     public async Task<IEnumerable<StockModel>> GetStocksAsync(string filePath)
     {
         using var reader = new StreamReader(filePath);
+
+        // Solution to exception when reading blank line taken from https://stackoverflow.com/a/57994196
         var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
         {
             AllowComments = true,
-            Comment = '"'
+            Comment = '"',
+            HasHeaderRecord = true,
+            HeaderValidated = null,
+            MissingFieldFound = null,
+            ShouldSkipRecord = args => args.Row.Parser.Record.All(string.IsNullOrWhiteSpace)
         };
         using var csv = new CsvReader(reader, configuration);
         csv.Context.RegisterClassMap<StockModelMap>();
