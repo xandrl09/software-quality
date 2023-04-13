d5a0f933d1292723762c19735f02caffdd836829
Samuel Tóth <mr.samueltoth@gmail.com>
2023-04-13 16:37:03 +0200

    Added more tests to HoldingsDifferenceTests

--- Stocks/StocksTests/HoldingsDifferenceTests.cs
+++ Stocks/StocksTests/HoldingsDifferenceTests.cs
@@ -136,6 +136,80 @@ public class HoldingsDifferenceTests
         });
     }
 
+     [Test]
+    public void GetDifference_StockPositionReducedAndIncreased_ReturnsCorrectPositions()
+    {
+        // arrange
+        var newReport = new List<StockModel> {
+            new StockModel { Cusip = "594918104", Ticker = "MSFT", Company = "Microsoft", Shares = 10, Weight = "10%" },
+            new StockModel { Cusip = "38259P706", Ticker = "GOOG", Company = "Google", Shares = 50, Weight = "50%" },
+            new StockModel { Cusip = "123456789", Ticker = "AMZN", Company = "Amazon", Shares = 15, Weight = "15%" },
+            new StockModel { Cusip = "22291829E", Ticker = "AAPL", Company = "Apple", Shares = 20, Weight = "20%" },
+            new StockModel { Cusip = "01KW31203", Ticker = "NVDA", Company = "NVIDIA", Shares = 5, Weight = "5%" },
+        };
+
+        // act
+        var result = _holdingsDifferenceService.GetDifference(newReport, _oldReport);
+
+        // assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.IncreasedPositons.Count, Is.EqualTo(2));
+            Assert.That(result.IncreasedPositons.FirstOrDefault()?.Ticker, Is.EqualTo("GOOG"));
+            Assert.That(result.IncreasedPositons.FirstOrDefault()?.CompanyName, Is.EqualTo("Google"));
+            Assert.That(result.IncreasedPositons.FirstOrDefault()?.DifferenceInShares, Is.EqualTo(20));
+            Assert.That(result.IncreasedPositons.FirstOrDefault()?.PercentageDifferenceInShares, Is.EqualTo(66.67));
+            Assert.That(result.IncreasedPositons.FirstOrDefault()?.Weight, Is.EqualTo("50%"));
+
+            Assert.That(result.ReducedPositions.Count, Is.EqualTo(3));
+            Assert.That(result.ReducedPositions.ElementAt(0).Ticker, Is.EqualTo("MSFT"));
+            Assert.That(result.ReducedPositions.ElementAt(0).CompanyName, Is.EqualTo("Microsoft"));
+            Assert.That(result.ReducedPositions.ElementAt(0).DifferenceInShares, Is.EqualTo(-5));
+            Assert.That(result.ReducedPositions.ElementAt(0).PercentageDifferenceInShares, Is.EqualTo(-33.33));
+            Assert.That(result.ReducedPositions.ElementAt(0).Weight, Is.EqualTo("10%"));
+
+            Assert.That(result.ReducedPositions.ElementAt(1).Ticker, Is.EqualTo("AAPL"));
+            Assert.That(result.ReducedPositions.ElementAt(1).CompanyName, Is.EqualTo("Apple"));
+            Assert.That(result.ReducedPositions.ElementAt(1).DifferenceInShares, Is.EqualTo(-5));
+            Assert.That(result.ReducedPositions.ElementAt(1).PercentageDifferenceInShares, Is.EqualTo(-20));
+            Assert.That(result.ReducedPositions.ElementAt(1).Weight, Is.EqualTo("20%"));
+
+            Assert.That(result.NewPositions.Count, Is.EqualTo(0));
+        });
+    }
+
+    [Test]
+    public void GetDifference_TwoPositionsRemovedAndOneAdded_ReturnsCorrectResults()
+    {
+        // arrange
+        var newReport = new List<StockModel> {
+            new StockModel { Cusip = "594918104", Ticker = "MSFT", Company = "Microsoft", Shares = 15, Weight = "18%" },
+            new StockModel { Cusip = "123456789", Ticker = "AMZN", Company = "Amazon", Shares = 10, Weight = "12%" },
+            new StockModel { Cusip = "01KW31203", Ticker = "NVDA", Company = "NVIDIA", Shares = 20, Weight = "23%" },
+            new StockModel { Cusip = "123456780", Ticker = "FB", Company = "Facebook", Shares = 40, Weight = "47%" },
+        };
+
+        // act
+        var result = _holdingsDifferenceService.GetDifference(newReport, _oldReport);
+
+        // assert
+        Assert.Multiple(() =>
+        {
+            // check for removed positions
+            Assert.That(result.ReducedPositions.Count, Is.EqualTo(2));
+            Assert.That(result.ReducedPositions.Any(p => p.Ticker == "GOOG"), Is.True);
+            Assert.That(result.ReducedPositions.Any(p => p.Ticker == "AAPL"), Is.True);
+
+            // check for added positions
+            Assert.That(result.NewPositions.Count, Is.EqualTo(1));
+            Assert.That(result.NewPositions.FirstOrDefault()?.Ticker, Is.EqualTo("FB"));
+            Assert.That(result.NewPositions.FirstOrDefault()?.Company, Is.EqualTo("Facebook"));
+            Assert.That(result.NewPositions.FirstOrDefault()?.Shares, Is.EqualTo(40));
+            Assert.That(result.NewPositions.FirstOrDefault()?.Weight, Is.EqualTo("47%"));
+
+        });
+    }
+
     [Test]
     public void GetDifference_FakeItEasy_FunctionCalled()
     {
