82b023d0d29ddd9d87e006d92693e31a3297fc22
xkonar <xkonar@fi.muni.cz>
2023-04-25 15:39:59 +0200

    feat: reworked ClientTests into required format

--- Stocks/StocksTests/MockedClient.cs
+++ Stocks/StocksTests/MockedClient.cs
@@ -5,6 +5,7 @@ using Stocks.Services.Client;
 using Stocks.Services.Diff;
 using Stocks.Services.Exceptions;
 using Stocks.Services.Files;
+using Stocks.Services.Models;
 using Stocks.Services.Models.Configuration;
 using Stocks.Services.Output;
 using Stocks.Services.Parsers;
@@ -14,7 +15,7 @@ namespace StocksTests;
 public class MockedClient
 {
     public Client TestClient { get; }
-    public StringWriter? ConsoleOutput { get; set; }
+    private StringWriter? ConsoleOutput { get; set; }
     private IDownloadService _download;
     private IFileService _dateFileService;
     private IParseService _parser;
@@ -66,16 +67,102 @@ public class MockedClient
         return this;
     }
     
+    public MockedClient DownloadEmptyData()
+    {
+        A.CallTo(() => _download.DownloadFile(_settings.CsvUrl)).Returns("");
+        return this;
+    }
+    
+    public MockedClient CanSaveContent()
+    {
+        A.CallTo(() => _dateFileService.SaveContent(A<string>.Ignored)).DoesNothing();
+        return this;
+    }
+    public MockedClient CanNotSaveContent()
+    {
+        A.CallTo(() => _dateFileService.SaveContent(A<string>.Ignored)).Throws<IOException>();
+        return this;
+    }
+    
+    public MockedClient CanGetLastAvailableFilePath()
+    {
+        A.CallTo(() => _dateFileService.GetLastAvailableFilePath()).Returns("valid path");
+        return this;
+    }
+    
+    public MockedClient CanNotGetLastAvailableFilePath()
+    {
+        A.CallTo(() => _dateFileService.GetLastAvailableFilePath()).Throws<CsvFilePathNotFoundException>();
+        return this;
+    }
+    
+    public MockedClient CanNotParseData()
+    {
+        A.CallTo(() => _parser.GetStocksAsync(A<string>.Ignored)).Throws<MissingFieldException>();
+        return this;
+    }
+    
+    public MockedClient CanParseData()
+    {
+        var stocksList = new List<StockModel>();
+        var stockModel = new StockModel
+            { Cusip = "594918104", Ticker = "MSFT", Company = "Microsoft", Shares = 15, Weight = "15%" };
+        stocksList.Add(stockModel);
+
+        HoldingsDifferenceModel model = new HoldingsDifferenceModel();
+        model.NewPositions = stocksList;
+        model.IncreasedPositons = new List<StockDifferenceModel>();
+        model.ReducedPositions = new List<StockDifferenceModel>();
+        
+        A.CallTo(() => _parser.GetStocksAsync(A<string>.Ignored)).Returns(stocksList);
+        return this;
+    }
+    
+    public MockedClient GetLastAvailableFilePathThrowsException()
+    {
+        A.CallTo(() => _dateFileService.GetLastAvailableFilePath()).Throws<CsvFilePathNotFoundException>();
+        return this;
+    }
+    
     public  MockedClient RunAsync()
     {
         TestClient.Run();
         return this;
     }
     
-    public void AssertException(string AssertError, string ActualOutput)
+    public  MockedClient CanGetDifference()
     {
-        Assert.That(AssertError,
-            Is.EqualTo(ActualOutput));
+        var stocksList = new List<StockModel>();
+        var stockModel = new StockModel
+            { Cusip = "594918104", Ticker = "MSFT", Company = "Microsoft", Shares = 15, Weight = "15%" };
+        stocksList.Add(stockModel);
+        
+        HoldingsDifferenceModel model = new HoldingsDifferenceModel();
+        model.NewPositions = stocksList;
+        model.IncreasedPositons = new List<StockDifferenceModel>();
+        model.ReducedPositions = new List<StockDifferenceModel>();
+        
+        A.CallTo(() =>
+                _differenceService.GetDifference(A<IEnumerable<StockModel>>.Ignored,
+                    A<IEnumerable<StockModel>>.Ignored))
+            .Returns(model);
+        return this;
+    }
+    
+    public void AssertException(string assertError)
+    {
+        Assert.That(assertError,
+            Is.EqualTo(GetConsoleOutput()));
+    }
+
+    public string GetConsoleOutput()
+    {
+        return ConsoleOutput.ToString().Trim();
+    }
+
+    public void AssertContainsStockInfo(string expected)
+    {
+        StringAssert.Contains(expected, GetConsoleOutput());
     }
 
 }
